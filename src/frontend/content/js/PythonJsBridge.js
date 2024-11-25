e74fbbaeb8e8bfdd5d0cb57a441185eefca1c24d
JeanMariePrevost <jeanmarie.prevost@outlook.com>
2024-11-24 20:14:00 -0500

    Add a note on how to listen to events from the backend for future reference

--- src/frontend/content/js/PythonJsBridge.js
+++ src/frontend/content/js/PythonJsBridge.js
@@ -5,3 +5,12 @@ export function sendDataToPython(eventType, data) {
   //Not sure if I,ll need special processing or error handling
   pywebview.api.send_event_to_python(eventType, data);
 }
+
+// NOTE : To receive from Python, you need to add an event listener to the window object
+// It is accessible from the global scope, simply do something like:
+// window.addEventListener("py_js_test_event", (event) => {
+//   console.log("Received payload: ", event.detail);
+//   // No parsing needed, the event.detail is already a JS object
+//   // You can access the data and any attributes with dot notation like this:
+//   console.log("Data received: ", event.detail.data);
+// });
