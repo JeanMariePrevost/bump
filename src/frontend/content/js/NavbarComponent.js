3068ebbe5ac43af243a5e3cd4b2cd685294ff975
JeanMariePrevost <jeanmarie.prevost@outlook.com>
2024-12-12 17:06:24 -0500

    Add documentation link to navbar; add icon for bundled exe

--- src/frontend/content/js/NavbarComponent.js
+++ src/frontend/content/js/NavbarComponent.js
@@ -8,4 +8,12 @@ export class NavbarComponent extends BaseComponent {
   constructor(parentSelector) {
     super(parentSelector, "navbar", "fragments/navbar.html");
   }
+
+  _onElementReady() {
+    // Listen for and catch the click event on the navbar "id=documentation-link" link to open as an external link
+    this.addManagedEventListener(document.getElementById("documentation-link"), "click", (event) => {
+      event.preventDefault();
+      window.open("https://jeanmarieprevost.github.io/bump/", "_blank");
+    });
+  }
 }
