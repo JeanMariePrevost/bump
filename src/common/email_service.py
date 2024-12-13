64d2df98a30a23c7e4939ca8e0c501ffdf7ef8b4
JeanMariePrevost <jeanmarie.prevost@outlook.com>
2024-12-13 12:41:01 -0500

    Improve SMTP error handling and logging

--- src/common/email_service.py
+++ src/common/email_service.py
@@ -12,7 +12,7 @@ def send_email_alert(subject, body):
     Send an email alert.
     """
     get_general_logger().debug("email_service.py sending email alert")
-    __send_smtp_email(subject, body, settings.to_email)
+    __send_smtp_email(subject, body, settings.smtp_target_email)
 
 
 def send_sms_alert(subject, body):
@@ -26,16 +26,25 @@ def send_sms_alert(subject, body):
 def __send_smtp_email(subject, body, target_email):
     # Create the email
     msg = MIMEMultipart()
-    msg["From"] = settings.smtp_username
-    msg["To"] = target_email
-    msg["Subject"] = subject
-    msg.attach(MIMEText(body, "plain"))
+    try:
+        msg["From"] = settings.smtp_username
+        msg["To"] = target_email
+        msg["Subject"] = subject
+        msg.attach(MIMEText(body, "plain"))
+    except Exception as e:
+        get_general_logger().error(f"Failed to create email: {e}")
+        return
 
     # Get the password from the keyring or from the user if it's not stored
     try:
         password = get_password_from_keyring_or_user(settings.smtp_username)
     except Exception as e:
-        print(f"Failed to get password: {e}")
+        get_general_logger().error(f"Failed to get SMTP password: {e}")
+        return
+
+    # If any requirement is missing, log and return
+    if not settings.smtp_server or not settings.smtp_port or not settings.smtp_username or not target_email or not password:
+        get_general_logger().warning("Trying to send email/SMS alerts, but SMTP settings are missing or invalid")
         return
 
     # Send the email
@@ -46,10 +55,5 @@ def __send_smtp_email(subject, body, target_email):
             server.login(settings.smtp_username, password)
             server.sendmail(settings.smtp_username, target_email, msg.as_string())
     except Exception as e:
-        get_general_logger().error(f"Failed to send email: {e}")
-
-
-if __name__ == "__main__":
-    # send_email_alert("Test Subject", "Test Body")
-    # send_sms_alert("Test Subject", "Test Body")
-    pass
+        get_general_logger().error(f"Could not perform SMTP operation: {e}")
+        return
